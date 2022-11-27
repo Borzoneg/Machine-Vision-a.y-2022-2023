2f505e52b7888b3c75aefb381818a8a35da2368a
Borzoneg <borzoneg@gmail.com>
2022-11-27 22:54:20 +0100

    Started exercise 07

---
+++ src/exercise_07.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+int main(int argc, char **argv) {
+    
+    return 0;
+}
